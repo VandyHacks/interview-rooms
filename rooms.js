5bb89260b7faed2031deb8966d3c47b46e6209cf
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-01-29 18:30:21 -0600

    Update rooms

--- rooms.js
+++ rooms.js
@@ -1,22 +1,112 @@
 export default [
     {
-        "date": "1/30/2018",
-        "rooms": [
+        date: '1/30/2018',
+        rooms: [
             {
-                "start": "4:25 PM",
-                "end": "7:25 PM",
-                "room": "FGH 313"
+                start: '3:30 PM',
+                end: '8:45 PM',
+                room: 'TBD - Check your email'
             }
         ]
     },
     {
-        "date": "02/01/2018",
-        "rooms": [
+        date: '01/31/2018',
+        rooms: [
             {
-                "start": "4:00 PM",
-                "end": "8:00 PM",
-                "room": "FGH 200"
+                start: '4:25 PM',
+                end: '8:30 PM',
+                room: 'FGH 313'
+            }
+        ]
+    },
+    {
+        date: '02/01/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '8:30 PM',
+                room: 'FGH 200'
+            }
+        ]
+    },
+    {
+        date: '02/02/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '8:00 PM',
+                room: 'FGH 200'
+            }
+        ]
+    },
+    {
+        date: '02/04/2018',
+        rooms: [
+            {
+                start: '2:00 PM',
+                end: '6:00 PM',
+                room: 'FGH 200'
+            }
+        ]
+    },
+    {
+        date: '02/05/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '5:00 PM',
+                room: 'FGH 110'
+            },
+            {
+                start: '5:00 PM',
+                end: '11:00 PM',
+                room: 'FGH 200'
+            }
+        ]
+    },
+    {
+        date: '02/06/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '6:00 PM',
+                room: 'FGH 138'
+            },
+            {
+                start: '6:00 PM',
+                end: '10:00 PM',
+                room: 'FGH 300'
+            }
+        ]
+    },
+    {
+        date: '02/07/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '6:00 PM',
+                room: 'FGH 134'
+            }
+        ]
+    },
+    {
+        date: '02/08/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '8:00 PM',
+                room: 'FGH 200'
+            }
+        ]
+    },
+    {
+        date: '02/09/2018',
+        rooms: [
+            {
+                start: '4:00 PM',
+                end: '8:00 PM',
+                room: 'FGH 200'
             }
         ]
     }
-]
+];
